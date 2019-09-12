7fc052819520e297da833eec6ca4eca6559a1c5f
Dasy92 <52685626+Dasy92@users.noreply.github.com>
2019-09-12 15:52:08 +0800

    Update sspanel.sh

--- sspanel.sh
+++ sspanel.sh
@@ -4,6 +4,6 @@ php composer.phar
 php composer.phar install
 cp config/.config.example.php config/.config.php
 cd ../
-chmod -R 755 你的文件夹名/
-chown -R www:www 你的文件夹名/
-ln -s /www/wwwroot/你的文件夹名/sql/glzjin_all.sql /www/backup/database/
+chmod -R 755 ss.ku.ink/
+chown -R www:www ss.ku.ink/
+ln -s /www/wwwroot/ss.ku.ink/sql/glzjin_all.sql /www/backup/database/
