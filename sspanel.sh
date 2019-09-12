b0f19b93723b919e5ced49e18e9960fba46f4922
Dasy92 <52685626+Dasy92@users.noreply.github.com>
2019-09-12 15:50:21 +0800

    Update sspanel.sh

--- sspanel.sh
+++ sspanel.sh
@@ -1,1 +1,9 @@
-git clone -b master https://github.com/Anankke/SSPanel-Uim.git tmp && mv tmp/.git . && rm -rf tmp && git reset --hard
+git config core.filemode false
+wget https://getcomposer.org/installer -O composer.phar
+php composer.phar
+php composer.phar install
+cp config/.config.example.php config/.config.php
+cd ../
+chmod -R 755 你的文件夹名/
+chown -R www:www 你的文件夹名/
+ln -s /www/wwwroot/你的文件夹名/sql/glzjin_all.sql /www/backup/database/
